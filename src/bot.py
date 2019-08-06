c033c8d7470eef5cc16c2517f6a703c1262db68e
Daniel R <daniel8rc@gmail.com>
2019-08-06 04:23:21 +0200

    Añadidas acciones al bot.

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,10 @@
 from dictionary import Dictionary
-from config.auth import token
+from config.settings import (token, lang)
 import telebot
-
+import sys
+import random
 bot = telebot.TeleBot(token)
+d = Dictionary(lang)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -10,6 +12,26 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    message.text = message.text.lower()
+    msg = 'err'
+    try:
+        if message.text == 'random':
+            n_key = random.randint(0,len(d.dictionary_json.keys()))
+            selected_key = list(d.dictionary_json.keys())[n_key]
+            msg = '%s => %s' % (selected_key, d.dictionary_json[selected_key])
+        elif '**' in message.text:
+            print("Edita")
+            message.text = message.text.replace('**edit**', '')
+            translation = message.text.split('-')
+            d.dictionary_json[translation[0]] = translation[1].lower()
+            d.update_dictionary()
+            msg = "Updated!"
+        else:
+            msg = d.dictionary_json[message.text.lower()]
+    except Exception as e:
+        print("Exception (echo_all) -> ", str(e))
+        pass
+    
+    bot.reply_to(message,msg)
 
 bot.polling()
