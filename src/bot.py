10134f46d76bf2521f86916384212f1266266a86
Daniel R <daniel8rc@gmail.com>
2019-08-07 02:00:10 +0200

    Add class for controller database and add class for analyze messages.

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,54 @@
 from dictionary import Dictionary
 from config.settings import (token, lang)
+from MySQLdb import _mysql
 import telebot
 import sys
 import random
+
 bot = telebot.TeleBot(token)
 d = Dictionary(lang)
+
+class Database():
+    def __init__(self):
+        self.host = host
+        self.port = port
+    
+    def stablish_connection(self):
+        db=_mysql.connect(host="localhost",user="joebob",
+                  passwd="moonpie",db="thangs")
+class Messages():
+    def __init__(self, msg):
+        self.msg = msg
+        self.reply_message = 'error'
+
+    def get_random_translation(self):
+        n_key = random.randint(0,len(d.dictionary_json.keys()))
+        selected_key = list(d.dictionary_json.keys())[n_key]
+        self.reply_message = '%s => %s' % (selected_key, d.dictionary_json[selected_key])
+
+
+    def edit_translation(self):
+        rows = self.msg.split('\n')
+        for row in rows:
+            translation = row.replace('**', '').split('-')
+            d.dictionary_json[translation[0].lower()] = translation[1].lower()
+            d.update_dictionary()
+            self.reply_message = "Updated!"
+
+    def analyze_message(self):
+        try:
+            if self.msg == 'random':
+                self.get_random_translation()
+            elif '**' in self.msg:
+                self.edit_translation()
+            else:
+                self.reply_message = d.dictionary_json[self.msg.lower()]
+        except Exception as e:
+            print("Exception (analyze_message) -> ", str(e))
+            pass
+
+        return self.reply_message
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
