b729441afa02100ce982baf2f3f7eb0f81e31722
Daniel R <daniel8rc@gmail.com>
2019-08-14 01:41:47 +0200

    Added dictionary controller

--- learn_bot/telegram_bot/views.py
+++ learn_bot/telegram_bot/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import tDictionary
+from rest_framework import (permissions, response, generics, status)
+from rest_framework.response import Response
 
 # Create your views here.
+
+class DictionaryController(generics.GenericAPIView):
+    def get(self, option):
+        self.response = {}
+        self.response['status'] = 400
+        self.status_response = status.HTTP_400_BAD_REQUEST
+
+        try:
+            if option == 'reload':
+                self.status_response = status.HTTP_200_OK
+                self.response['status'] = 200
+        except:
+            pass
+        
+        return Response(self.response, status=self.status_response)
+
+
+dictionary = DictionaryController.as_view()
